913e087611c0cc42f13a9a4e9c35757f9506b70b
Antony Redman <RumataYounger@gmail.com>
2022-12-12 21:59:44 +0300

    modifying conversion

--- log_input/dict_decoder.go
+++ log_input/dict_decoder.go
@@ -2,6 +2,8 @@ package log_input
 
 import (
   "log"
+  "strconv"
+  "fmt"
   "github.com/nlpodyssey/gopickle/types"
   "augeas/utils"
 )
@@ -13,7 +15,26 @@ func (self DictDecoder) Decode (input types.Dict) {
   logKeys := utils.MakeLogKeys()
   for i := 0; i <= input.Len(); i++ {
     val, _ := input.Get(logKeys[i])
-    logMap[logKeys[i]] = val.(string)
+    log.Print(val)
+    if val != nil {
+      switch val.(type) {
+      case int:
+        logMap[logKeys[i]] = string(val.(int))
+      case float32:
+        comma, _ := val.(float32)
+        vl := fmt.Sprintf("%f", comma)
+        logMap[logKeys[i]] = vl
+      case bool:
+        comma, _ := val.(bool)
+        logMap[logKeys[i]] = strconv.FormatBool(comma)
+      case string:
+        logMap[logKeys[i]] = val.(string)
+      default:
+        panic("undefined type")
+      }
+    } else {
+      logMap[logKeys[i]] = ""
+    }
   }
   log.Print(logMap)
 }
